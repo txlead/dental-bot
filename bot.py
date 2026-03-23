ec67b05befac75f534a28f992a7e47384fcbdede
Product Designer (DS and UX) <mateuszgrs96@gmail.com>
2026-03-23 22:22:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1284,7 +1284,7 @@ def home():
 
 def run_flask():
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
+  
 if __name__ == "__main__":
     bot.remove_webhook()
     t = threading.Thread(target=run_flask)
